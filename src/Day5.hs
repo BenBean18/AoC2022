cfe352f1a6be3ba4b5d4210453678168bd185091
BenBean18 <beakerthespeaker@gmail.com>
2022-12-05 09:39:24 -0500

    Day 5

---
+++ src/Day5.hs
@@ -0,0 +1,71 @@
+module Day5 where
+
+import Utilities
+import Text.Regex.Base
+import Text.Regex.PCRE
+import Data.List
+import Debug.Trace
+
+debug = flip trace
+
+insert1D :: Int -> a -> [a] -> [a]
+insert1D i v l = let (ys,zs) = splitAt i l in ys ++ [v] ++ zs
+
+set1D :: Int -> a -> [a] -> [a]
+set1D i v l = let (ys,zs) = splitAt i l in ys ++ [v] ++ (tail zs)
+
+-- we're going to ignore the zeroth index of the crates and start at 1
+addCrateStack :: [[Char]] -> String -> [[Char]]
+addCrateStack stacks "" = stacks
+addCrateStack stacks str = 
+    if (str !! 1) /= '1' && ((str !! 0) == ' ' || (str !! 0) == '[') then
+        let matches = (str ++ "\n") =~ "(\\[[A-Z]\\]|   )[ \\n]" :: [[String]]
+            crateIndex = 9 - ((length (str ++ "\n")) `div` 4) + 1
+            crate = ((matches !! 0) !! 1) !! 1 in
+        -- each of the matches represents this row of crates
+        -- we want to add each existent crate to stacks
+        -- i guess use recursion?
+        if crate == ' ' then addCrateStack stacks (drop 4 str)
+        else addCrateStack (set1D crateIndex ((stacks !! crateIndex) ++ [crate]) stacks) (drop 4 str)
+    else stacks
+
+parseInstruction :: [[Char]] -> String -> [[Char]]
+parseInstruction stacks str = 
+    let matches = str =~ "move ([0-9]+) from ([0-9]+) to ([0-9]+)" :: [[String]] in
+        if length matches /= 1 || length (matches !! 0) /= 4 then stacks -- invalid line, probably going through the start of the list
+        else 
+            let fromIndex = (read ((matches !! 0) !! 2)) :: Int
+                count = (read ((matches !! 0) !! 1)) :: Int
+                toIndex = (read ((matches !! 0 ) !! 3)) :: Int
+                from = reverse (take count (stacks !! fromIndex))
+                newColumnFrom = drop count (stacks !! fromIndex)
+                newColumnTo = from ++ (stacks !! toIndex)
+                in (set1D toIndex newColumnTo (set1D fromIndex newColumnFrom stacks))
+
+-- use folds
+part1 = do
+    lines <- getLines "day5/input.txt"
+    let stacks = (foldl parseInstruction (foldl addCrateStack [[],[],[],[],[],[],[],[],[],[]] lines) lines)
+    let heads = map head (tail stacks) -- ignore index 0
+    print heads
+
+-- Part 2
+
+parseInstruction2 :: [[Char]] -> String -> [[Char]]
+parseInstruction2 stacks str = 
+    let matches = str =~ "move ([0-9]+) from ([0-9]+) to ([0-9]+)" :: [[String]] in
+        if length matches /= 1 || length (matches !! 0) /= 4 then stacks -- invalid line, probably going through the start of the list
+        else 
+            let fromIndex = (read ((matches !! 0) !! 2)) :: Int
+                count = (read ((matches !! 0) !! 1)) :: Int
+                toIndex = (read ((matches !! 0 ) !! 3)) :: Int
+                from = take count (stacks !! fromIndex) -- not reversed for part 2
+                newColumnFrom = drop count (stacks !! fromIndex)
+                newColumnTo = from ++ (stacks !! toIndex)
+                in (set1D toIndex newColumnTo (set1D fromIndex newColumnFrom stacks))
+
+part2 = do
+    lines <- getLines "day5/input.txt"
+    let stacks = (foldl parseInstruction2 (foldl addCrateStack [[],[],[],[],[],[],[],[],[],[]] lines) lines)
+    let heads = map head (tail stacks) -- ignore index 0
+    print heads
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment
 import qualified Day4
+import qualified Day5
 
 main :: IO ()
 main = do
@@ -11,9 +12,11 @@ main = do
             day = (read (args !! 0) :: Int) in
                 if part == 1 then
                     if day == 4 then Day4.part1
-                    else print "I haven't solved that yet (or it doesn't exist)"
+                    else if day == 5 then Day5.part1
+                    else putStrLn "I haven't solved that yet (or it doesn't exist)"
                 else if part == 2 then
                     if day == 4 then Day4.part2
-                    else print "I haven't solved that yet (or it doesn't exist)"
-                else print "Usage: AoC2022 <day> <part>"
-    else print "Usage: AoC2022 <day> <part>"
+                    else if day == 5 then Day5.part2
+                    else putStrLn "I haven't solved that yet (or it doesn't exist)"
+                else putStrLn "Usage: AoC2022 <day> <part>"
+    else putStrLn "Usage: AoC2022 <day> <part>"

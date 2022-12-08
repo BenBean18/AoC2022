4ad47d989e046e58ae93b879b378284f31c12341
BenBean18 <beakerthespeaker@gmail.com>
2022-12-07 19:53:18 -0500

    Day 7 cleaned up code, added visualization

--- src/Day7.hs
+++ src/Day7.hs
@@ -5,23 +5,35 @@ import qualified Data.Map as Map
 import qualified Data.Map.Strict as Map.Strict
 import Text.Regex.Base
 import Text.Regex.PCRE
-import Debug.Trace
+import Data.Maybe
+import Control.Concurrent
+import System.IO
 
 -- I think creating the whole directory tree would be inefficient
 
+-- So we're just storing a dictionary<path, size>.
+-- When we add a new file, it recurses upward through the tree and adds the
+-- size to all of those directories.
+
+-- Custom datatype for filesystem state, has the abovementioned size map &
+-- current path
 data FSState = FSState { path :: String, sizeMap :: Map.Map String Int }
 
+-- remove the last directory from the path
+-- e.g. upOne /ab/cd/efg == /ab/cd
 upOne :: FSState -> FSState
 upOne fss = 
     let p = '/' : ((path fss) =~ "(([a-z]+)\\/)+" :: String) in
         if p == "/" then FSState { path = p, sizeMap = (sizeMap fss) }
         else FSState { path = init p, sizeMap = (sizeMap fss) }
 
+-- carry out a directory change
 parseCd :: FSState -> String -> FSState
 parseCd fss cdCmd =
-    if cdCmd == ".." then upOne fss -- remove last dir from path
-    else if cdCmd == "/"  then FSState {path = "/", sizeMap = (sizeMap fss)}
+    if cdCmd == ".." then upOne fss -- remove last dir from path and cd to it
+    else if cdCmd == "/" then FSState {path = "/", sizeMap = (sizeMap fss)} -- go to /
     else
+        -- this avoids a path with // in it which messes everything up
         if (path fss) == "/" then FSState {path = (path fss) ++ (cdCmd), sizeMap = (sizeMap fss)}
         else FSState {path = (path fss) ++ ('/' : cdCmd), sizeMap = (sizeMap fss)}
 
@@ -33,22 +45,23 @@ addSizeToMap fss size =
     -- This sets the path to / when it is done, so the path is set to / after a size is added.
     -- I switched to only using the map from this function in the main one.
     if (path fss) == "/" then
+        -- if we've gotten to the top level return
         let m = Map.Strict.insertWith (+) (path fss) size (sizeMap fss) in FSState {path = (path fss), sizeMap = m}
     else
+        -- otherwise add the size to the current directory and add it to
+        -- the next one up and so on until we recurse to / and trigger line 49
         let 
             newPath = path (upOne fss)
             m = Map.Strict.insertWith (+) (path fss) size (sizeMap fss)
             in addSizeToMap (FSState {path = newPath, sizeMap = m}) size
 
-debug = flip trace
-
--- expects command without "$ "
+-- expects command
 runCmd :: FSState -> String -> FSState
 runCmd fss out =
-    let cmd = take 4 out in
-        if cmd == "$ cd" then
+    let cmd = take 4 out in -- cmd = first 4 chars
+        if cmd == "$ cd" then -- "$ cd" == cd
             let arg = drop 5 out in parseCd fss arg
-        else if cmd == "$ ls" then
+        else if cmd == "$ ls" then -- "$ ls" == ls
             -- do nothing, we'll handle that when we get the files
             fss
         else if cmd == "dir " then -- will be "dir "
@@ -57,28 +70,43 @@ runCmd fss out =
             fss
         else
             -- yay file!
-            let match = out =~ "(\\d+)" :: String in
+            let match = out =~ "(\\d+)" :: String in -- match all digits (the size is a number)
+                -- return a new FSState using the current path and the size from addSizeToMap
                 FSState { path = (path fss), sizeMap = sizeMap (addSizeToMap fss (read match :: Int)) }
+
+-- print filesystem state (current directory + its size)
+printState :: FSState -> IO ()
+printState fss = 
+    let pathLen = length $ "\r" ++ (path fss)
+        pwdSize = fromMaybe 0 (sizeMap fss Map.!? path fss)
+        sizeLen  = length (show pwdSize) in do
+            putStr $ "\r" ++ (path fss) ++ (replicate (80 - pathLen) ' ') ++ " = " ++ (show pwdSize) ++ " bytes" ++ (replicate (20 - sizeLen) ' ')
+            hFlush stdout
 
 recurseAccumulate :: FSState -> [String] -> IO FSState
 recurseAccumulate fss [] = 
     do return fss
 recurseAccumulate fss list = 
     do
-        -- putStrLn ((path fss) ++" " ++ (show $ sizeMap fss) ++ " " ++ (head list))
+        printState fss
+        threadDelay 500000 -- 0.5 seconds
         (recurseAccumulate (runCmd fss (head list)) (drop 1 list))
 
 part1 = do
     lines <- getLines "day7/input.txt"
-    fss <- recurseAccumulate FSState { path = "/", sizeMap = Map.empty } lines
+    -- fold takes a function, list, and initial value. the function takes in
+    -- the current value and outputs a new one based on the list item.
+    -- fold returns the final value. l means from the left.
+    -- recurseAccumulate is basically an implementation of fold
+    let fss = foldl runCmd FSState { path = "/", sizeMap = Map.empty } lines
     -- let soln = fold 
-    print $ sum $ Map.map (\x -> if x <= 100000 then x else 0) (sizeMap fss)
+    putStrLn $ "Sum of the sizes of all directories under 100000: " ++ (show $ sum $ Map.filter (<= 100000) (sizeMap fss))
 
 -- 24390891 is wrong
 
 part2 = do
     lines <- getLines "day7/input.txt"
-    fss <- recurseAccumulate FSState { path = "/", sizeMap = Map.empty } lines
+    let fss = foldl runCmd FSState { path = "/", sizeMap = Map.empty } lines
     let driveSize = 70000000
     let updateSize = 30000000
     let usedSpace = ((sizeMap fss) Map.! "/")
@@ -94,4 +122,18 @@ part2 = do
 
     -- fixed it with some more thinking, logic, and `let`s
 
-    print $ minimum (Map.filter (>= minSpaceToFree) (sizeMap fss))
+    putStrLn $ "Smallest directory size that frees enough space: " ++ (show $ minimum (Map.filter (>= minSpaceToFree) (sizeMap fss)))
+
+-- Visualization
+vis = do
+    lines <- getLines "day7/input.txt"
+    fss <- recurseAccumulate FSState { path = "/", sizeMap = Map.empty } lines
+    let driveSize = 70000000
+    let updateSize = 30000000
+    let usedSpace = ((sizeMap fss) Map.! "/")
+    let unusedSpace = driveSize - usedSpace
+    let minSpaceToFree = updateSize - unusedSpace
+    putStrLn ""
+    putStrLn ("Used space: " ++ (show ((sizeMap fss) Map.! "/")) ++ "\nNeed to free at least " ++ (show minSpaceToFree) ++ " bytes")
+
+    putStrLn $ "Smallest directory size that frees enough space: " ++ (show $ minimum (Map.filter (>= minSpaceToFree) (sizeMap fss)))
--- src/Main.hs
+++ src/Main.hs
@@ -10,19 +10,22 @@ main :: IO ()
 main = do
     args <- getArgs
     if length args == 2 then
-        let part = (read (args !! 1) :: Int)
+        let part = args !! 1
             day = (read (args !! 0) :: Int) in
-                if part == 1 then
+                if part == "1" then
                     if day == 4 then Day4.part1
                     else if day == 5 then Day5.part1
                     else if day == 6 then Day6.part1
                     else if day == 7 then Day7.part1
                     else putStrLn "I haven't solved that yet (or it doesn't exist)"
-                else if part == 2 then
+                else if part == "2" then
                     if day == 4 then Day4.part2
                     else if day == 5 then Day5.part2
                     else if day == 6 then Day6.part2
                     else if day == 7 then Day7.part2
                     else putStrLn "I haven't solved that yet (or it doesn't exist)"
-                else putStrLn "Usage: AoC2022 <day> <part>"
-    else putStrLn "Usage: AoC2022 <day> <part>"
+                else if part == "v" then
+                    if day == 7 then Day7.vis
+                    else putStrLn "I haven't visualized that day yet (or it doesn't exist)"
+                else putStrLn "Usage: AoC2022 <day> <part || \"v\" for visualization>"
+    else putStrLn "Usage: AoC2022 <day> <part || \"v\" for visualization>"

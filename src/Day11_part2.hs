5ca4fbcde19272b037eadd51cdf994b6e4b25460
BenBean18 <beakerthespeaker@gmail.com>
2022-12-12 15:15:08 -0500

    Day 11 optimization Now ~600ms instead of ~5000ms It's still parsing the monkeys every time which seems super extraneous but at least now it only uses regexes once each

--- src/Day11_part2.hs
+++ src/Day11_part2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Day11_part2 where
 
 -- FRICK
@@ -32,7 +34,7 @@ import Data.Maybe
 import Text.Read
 
 -- Declare custom datatypes
-data Monkey = Monkey { items :: [Int], divisor :: Int, fn :: ([Monkey] -> [Monkey]), interactedCount :: Int }
+data Monkey = Monkey { items :: ![Int], divisor :: !Int, fn :: !([Monkey] -> [Monkey]), interactedCount :: !Int }
 
 instance Ord Monkey where
     m1 `compare` m2 = (interactedCount m1) `compare` (interactedCount m2)
@@ -57,7 +59,7 @@ Monkey 0:
 -}
 
 multiplyMods :: [Monkey] -> Int
-multiplyMods monkeys = (foldl (*) 1 $ map divisor monkeys)
+multiplyMods !monkeys = (foldl (*) 1 $! map divisor monkeys)
 
 -- math
 crtSimplify :: Int -> Int -> Int
@@ -66,46 +68,42 @@ crtSimplify modsMultiplied worryLevel =
 
 -- Parse operation like "new = old * 19" and simplifies using CRT instead of /3
 -- only uses * and + (won't decrease your worry level...)
-parseOperation :: String -> ([Monkey] -> Int -> Int)
-parseOperation op monkeys a =
-    let matches = op =~ "old ([*+]) (\\d+|(?:old))"
-        modsMultiplied = multiplyMods monkeys in
-        if length matches < 1 then error "Invalid monkey! Monkey business detected!"
-        else
-            let operator = head ((head matches) !! 1)
-                operand  = readMaybe ((head matches) !! 2) :: Maybe Int in
-                    if isNothing operand then -- it is "old"
-                        if operator == '*' then crtSimplify modsMultiplied (a * a)
-                        else if operator == '+' then crtSimplify modsMultiplied (a + a)
-                        else error "Invalid monkey! Monkey business detected!"
-                    else
-                        if operator == '*' then crtSimplify modsMultiplied (a * (fromJust operand))
-                        else if operator == '+' then crtSimplify modsMultiplied (a + (fromJust operand))
-                        else error "Invalid monkey! Monkey business detected!"
+parseOperation :: [[String]] -> Int -> (Int -> Int)
+parseOperation matches modsMultiplied a =
+    if length matches < 1 then error "Invalid monkey! Monkey business detected!"
+    else
+        let operator = head ((head matches) !! 1)
+            operand  = readMaybe ((head matches) !! 2) :: Maybe Int in
+                if isNothing operand then -- it is "old"
+                    if operator == '*' then crtSimplify modsMultiplied (a * a)
+                    else if operator == '+' then crtSimplify modsMultiplied (a + a)
+                    else error "Invalid monkey! Monkey business detected!"
+                else
+                    if operator == '*' then crtSimplify modsMultiplied (a * (fromJust operand))
+                    else if operator == '+' then crtSimplify modsMultiplied (a + (fromJust operand))
+                    else error "Invalid monkey! Monkey business detected!"
 
 getDivisor :: String -> Int
 getDivisor s =
     read (last (head (s =~ "divisible by (\\d+)" :: [[String]]))) :: Int
 
-parseTest :: String -> (Int -> Bool)
-parseTest s i =
-    let divisor = getDivisor s in i `mod` divisor == 0
-
 debug = flip trace
 
 -- Arguments: operation, test, monkey if true, monkey if false, monkey id
 -- Returns a function [Monkey] -> [Monkey]
-monkFunc :: ([Monkey] -> Int -> Int) -> (Int -> Bool) -> Int -> Int -> Int -> ([Monkey] -> [Monkey])
+monkFunc :: (Int -> Int) -> (Int -> Bool) -> Int -> Int -> Int -> ([Monkey] -> [Monkey])
 monkFunc operation test true false id monkeys =
     let currentItems = items (monkeys !! id) in
         if length currentItems == 0 then monkeys
         else
-            let thisItem = operation monkeys (head currentItems)
+            let thisMonkey = (monkeys !! id)
+                thisItem = operation (head currentItems)
                 monkeyToThrowTo = if test thisItem then true else false
-                destMonkeyItems = items (monkeys !! monkeyToThrowTo)
+                destMonkey = (monkeys !! monkeyToThrowTo)
+                destMonkeyItems = items destMonkey
                 newDestMonkeyItems = insert1D 0 thisItem destMonkeyItems
                 newSelfItems = tail currentItems
-                newMonkeys = (set1D id Monkey { items = newSelfItems, divisor = divisor (monkeys !! id), fn = fn (monkeys !! id), interactedCount = interactedCount (monkeys !! id) + 1 } (set1D monkeyToThrowTo Monkey { items = newDestMonkeyItems, divisor = divisor (monkeys !! monkeyToThrowTo), fn = fn (monkeys !! monkeyToThrowTo), interactedCount = interactedCount (monkeys !! monkeyToThrowTo) } monkeys)) in
+                newMonkeys = (set1D id thisMonkey { items = newSelfItems, interactedCount = interactedCount (thisMonkey) + 1 } (set1D monkeyToThrowTo destMonkey { items = newDestMonkeyItems } monkeys)) in
                     monkFunc operation test true false id newMonkeys
 
 -- parseMonkey takes a whole monkey:
@@ -117,21 +115,29 @@ Monkey 0:
     If true: throw to monkey 2
     If false: throw to monkey 3
 -}
-parseMonkey :: String -> Monkey
-parseMonkey monkey =
+parseMonkey :: String -> Int -> Monkey
+parseMonkey !monkey !modsMultiplied =
     let id = read (last (head (monkey =~ "Monkey (\\d+):" :: [[String]]))) :: Int
         startingItemsRaw = monkey =~ "Starting items: .+" :: String
         startingItems = map (\x -> read (last x) :: Int) (startingItemsRaw =~ "(\\d+)(?:[, ]*)" :: [[String]])
         operationRaw = monkey =~ "Operation: .+" :: String
-        operation = parseOperation operationRaw
+        operation = parseOperation (operationRaw =~ "old ([*+]) (\\d+|(?:old))") modsMultiplied
         testRaw = monkey =~ "Test: .+" :: String
-        test = parseTest monkey
+        divisor = getDivisor monkey
+        test = (\i -> i `mod` divisor == 0)
         true = read (last (head (monkey =~ "If true: throw to monkey (\\d+)" :: [[String]]))) :: Int
-        false = read (last (head (monkey =~ "If false: throw to monkey (\\d+)" :: [[String]]))) :: Int in
-            Monkey { items = startingItems, divisor = getDivisor monkey, fn = monkFunc operation test true false id, interactedCount = 0 }
+        false = read (last (head (monkey =~ "If false: throw to monkey (\\d+)" :: [[String]]))) :: Int
+        thisMonkFunc = monkFunc operation test true false id in
+            Monkey { items = startingItems, divisor = divisor, fn = thisMonkFunc, interactedCount = 0 }
 
 parseMonkeys :: [String] -> [Monkey]
-parseMonkeys strs = map (parseMonkey . concat . (\y -> map (\x -> x ++ "\n") y)) (divvy 6 7 strs)
+parseMonkeys strs =
+    let divviedStrs = (divvy 6 7 strs)
+        concattedStrs = map (concat . (\y -> map (\x -> x ++ "\n") y)) divviedStrs
+        firstMonkeys = map (\x -> parseMonkey x 0) concattedStrs
+        modsMultiplied = multiplyMods firstMonkeys in
+            map (\x -> parseMonkey x modsMultiplied) concattedStrs
+
 
 monkeyRound' :: [Monkey] -> Int -> [Monkey]
 monkeyRound' monkeys index =
@@ -149,7 +155,7 @@ monkeyRounds monkeys numRounds =
 sortDesc = sortBy (flip compare)
 
 part2' lines = do
-    let monkeys = parseMonkeys lines
+    let !monkeys = parseMonkeys lines
     let monkeysAfter10000 = monkeys `monkeyRounds` 10000
     print monkeysAfter10000
     let sorted = sortDesc monkeysAfter10000

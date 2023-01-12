55ba9d99f234b21d1a3a2e8c085f72dd1c33cfb9
BenBean18 <beakerthespeaker@gmail.com>
2023-01-12 11:20:10 -0500

    Day 19 in progress Can now get blueprint neighbors

--- src/Day19.hs
+++ src/Day19.hs
@@ -3,6 +3,10 @@ module Day19 where
 import qualified Data.Map as Map
 import Text.Regex.Base
 import Text.Regex.PCRE
+import Utilities
+import Data.List.Split
+import Data.List (uncons, unzip)
+import Data.Maybe
 
 -- idea: work backwards from obsidian-collecting robot
 -- most geodes = most obsidian-collecting robots
@@ -44,7 +48,7 @@ newState :: State -> Robot -> Blueprint -> State
 newState State { robots = rs, resources = rc } r bp = State { robots = r:rs, resources = rc `subtractResources` (bp Map.! r) }
 
 availableRobots :: ResourceCount -> Blueprint -> [Robot] -> [Robot]
-availableRobots rc bp rs = filter (\robot -> (==) 0 $ length $ filter (> 0) (Map.elems $ subtractResources rc (bp Map.! robot))) rs
+availableRobots rc bp rs = filter (\robot -> (length $ filter (< 0) (Map.elems $ subtractResources rc (bp Map.! robot))) == 0) rs
 
 mineralsToRC :: [Mineral] -> ResourceCount
 mineralsToRC ms = Map.fromListWith (+) (map (\x -> (x, 1)) ms)
@@ -55,7 +59,8 @@ neighbors State { robots = rs, resources = rc } bp =
         newRobotPossibilities = availableRobots newResources bp [Robot Ore, Robot Clay, Robot Obsidian, Robot Geode] in
             -- 1. find robots which can be created (mineral count > mineral need)
             -- 2. get states with those robots
-            map (\robot -> State { robots = robot:rs, resources = subtractResources newResources (bp Map.! robot) }) newRobotPossibilities -- ugh, need to subtract resources from newResources for every robot...
+            -- there is also the state where just collection happens
+            State { robots = rs, resources = newResources } : map (\robot -> State { robots = robot:rs, resources = subtractResources newResources (bp Map.! robot) }) newRobotPossibilities -- ugh, need to subtract resources from newResources for every robot...
 
 stringToMineral :: String -> Mineral
 stringToMineral "ore" = Ore
@@ -84,4 +89,20 @@ parseBlueprint (l:ls) blueprint =
         else if len == 1 then parseBlueprint ls (Map.insert (Robot Obsidian) val blueprint)
         else if len == 0 then parseBlueprint ls (Map.insert (Robot Geode) val blueprint)
         else error "the heck?"
+-- oops...blueprints look like "Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 20 clay. Each geode robot costs 2 ore and 12 obsidian."
 
+emptyRC :: ResourceCount
+emptyRC = Map.fromList [(Ore, 0), (Clay, 0), (Obsidian, 0), (Geode, 0)]
+
+part1 = do
+    lines_ <- getLines "day19/input.txt"
+    let raw = map (splitOneOf ":.") lines_
+    let (ids, prints_) = unzip $ map (fromJust . uncons) raw
+    let prints = map init prints_
+    let blueprints = map (flip parseBlueprint $ Map.empty) prints
+    let neighborsOfFirst = neighbors (State { robots = [Robot Ore], resources = Map.insert Ore 2 emptyRC }) (head blueprints)
+    print $ neighborsOfFirst
+    -- let bp = head blueprints
+    -- print $ (length $ filter (>= 0) (Map.elems $ subtractResources (Map.fromList [(Ore, 4)]) (bp Map.! Robot Ore))) > 0
+    -- print $ addResources Map.empty $ mineralsToRC $ map mineral [Robot Ore]
+    -- print $ availableRobots (Map.fromList [(Ore, 1)]) (head blueprints) [Robot Ore, Robot Clay, Robot Obsidian, Robot Geode]
